0b23d8bcd585e7be52346d874ca8fccea239c0f6
Melvin Flores <flores.majano@gmail.com>
2018-12-10 10:36:34 -0600

    proyecto de ejemplo usando LINQ

---
+++ ejemploLINQ/Controllers/LinqController.cs
@@ -0,0 +1,75 @@
+﻿using ejemploLINQ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ejemploLINQ.Controllers
+{
+    public class LinqController : Controller
+    {
+        //instancia de la clase estudiante
+        Estudiantes Db = new Estudiantes();
+        // GET: Linq
+
+       
+        public ActionResult Index()
+        {
+            
+            var listado = Db.Lista();
+            return View(listado.ToList());
+        }
+        [HttpPost]
+        public ActionResult Index(string txtbuscar, string select, string rd)
+        {
+            var listado = Db.Lista();
+            string formato = txtbuscar.Trim();
+            var query = from a in Db.Lista() select a;
+            #region usandoSelect
+            if (formato != "")
+            {
+                if (select == "0")
+                {
+                    if (select == "1")
+                    {
+                        query = from n in Db.Lista() where n.Nombre.Contains(formato) select n;
+                    }
+                    if (select == "2")
+                    {
+                        query = from n in Db.Lista() where n.Materia.Contains(formato) select n;
+                    }
+                    if (select == "3")
+                    {
+                        double prom = Convert.ToDouble(formato);
+                        query = from n in Db.Lista() where n.Promedio == prom select n;
+                    }
+                }
+
+            }
+            #endregion
+            #region usandoRadio
+            if (formato != "")
+            {
+                if (rd == "1")
+                {
+                    query = from n in Db.Lista() where n.Nombre.Contains(formato) select n;
+                }
+                if (rd == "2")
+                {
+                    query = from n in Db.Lista() where n.Materia.Contains(formato) select n;
+                }
+                if (rd == "3")
+                {
+                    double prom = Convert.ToDouble(formato);
+                    query = from n in Db.Lista() where n.Promedio == prom select n;
+                }
+            }
+            #endregion
+
+            //var query2 = (from p in listado where p.Materia.Contains(txtbuscar) select p);
+            return View(query.ToList());
+        }
+
+    }
+}
---
+++ ejemploLINQ/Models/Estudiantes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ejemploLINQ.Models
+{
+    public class Estudiantes
+    {
+        public int Carnet { get; set; }
+        public string Nombre { get; set; }
+        public string Materia { get; set; }
+        public double Promedio { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+        public List<Estudiantes> Lista()
+        {
+            var estudiante1 = new Estudiantes()
+            {
+                Carnet = 001,
+                Nombre ="Maria",
+                Materia = "gestion bases de datos",
+                Promedio = 8.5,
+                FechaRegistro = DateTime.Now
+            };
+            var estudiante2 = new Estudiantes()
+            {
+                Carnet = 002,
+                Nombre ="Juan",
+                Materia = "Linq",
+                Promedio = 6,
+                FechaRegistro = DateTime.Now
+            };
+            var estudiante3 = new Estudiantes()
+            {
+                Carnet = 003,
+                Nombre = "Pedro",
+                Materia = "Matematica",
+                Promedio = 4,
+                FechaRegistro = DateTime.Now
+            };
+            var estudiante4 = new Estudiantes()
+            {
+                Carnet = 004,
+                Nombre = "Lorena",
+                Materia = "Linq",
+                Promedio = 6.5,
+                FechaRegistro = DateTime.Now
+            };
+            return new List<Estudiantes>() {estudiante1, estudiante2, estudiante3, estudiante4 };
+        }
+    }
+
+   
+}
